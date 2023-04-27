6847e59e73e6212f21fa5207c2caae732355e9d0
yar2001 <yar2001@163.com>
2023-04-27 13:45:31 +0800

    up

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import httpProxy from 'http-proxy';
+import cors from 'cors';
 
 const port = 9000;
 
 const proxy = httpProxy.createProxyServer({});
 const app = express();
+
+app.use(cors());
 
 app.use('/:lang', (req, res) => {
   proxy.web(
